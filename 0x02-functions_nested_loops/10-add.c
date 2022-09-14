4f7b5deff558067ee4fa6d2db7610ef68f5b544c
Gudu-Diana <gududiana@gmail.com>
2022-09-14 13:28:17 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sums up two integers
+ * @num1: first int
+ * @num2: second int
+ *
+ * return: result of the sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-a program that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+a program that adds two integers and returns the result
